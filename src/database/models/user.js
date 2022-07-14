1069525587a24e19962c2a774cb9376fb980f65b
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-14 20:35:46 -0300

    :recycle: refactor: rename model files

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,6 +2,7 @@ const sequelize = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
