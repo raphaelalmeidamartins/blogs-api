6f9f8dc7c3b99bdfc8e331a77053ff9c469c6379
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-15 19:10:11 -0300

    :sparkles: feat: create errorMiddleware and a custom error
    Also add the login route

--- src/api.js
+++ src/api.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+const loginRoutes = require('./routes/loginRoutes');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', loginRoutes);
+
+app.use(errorMiddleware);
 
 // ...
 
---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,20 @@
+function errorMiddleware(err, _req, res, _next) {
+  const { name, message } = err;
+  switch (true) {
+    case ['ValidationError'].includes(name):
+      res.status(400).json({ message });
+      break;
+    case ['TokenExpiredError', 'JsonWebTokenError', 'NotBeforeError'].includes(name): {
+      res.status(401).json({ message: 'Expired or invalid token' });
+      break;
+    }
+    case ['NotFoundError'].includes(name):
+      res.status(400).json({ message });
+      break;
+    default:
+      console.warn(err);
+      res.sendStatus(500);
+  }
+}
+
+module.exports = errorMiddleware;
