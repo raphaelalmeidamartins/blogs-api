1c2c436c8c7e7e1b05a2972004ad8052ec492bf2
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-17 19:08:47 -0300

    :wrench: add config to show all sequelize methods

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -6,6 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
+/** @type {{ [key:string]: Sequelize.ModelStatic<Sequelize.Model<any, any>>}} */
 const db = {};
 
 let sequelize;
