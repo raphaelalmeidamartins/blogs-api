840f2cb6cee8e04d6a7f648a09793941df1bd659
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-18 16:32:12 -0300

    :books: docs: create documentation with Swagger UI

--- src/api.js
+++ src/api.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
+const swaggerUI = require('swagger-ui-express');
+const swaggerSettingsBr = require('./swagger-br.json');
+const swaggerSettingsEn = require('./swagger-en.json');
+
 const errorMiddleware = require('./middlewares/errorMiddleware');
+
 const categoriesRoutes = require('./routes/categoriesRoutes');
 const loginRoutes = require('./routes/loginRoutes');
 const postRoutes = require('./routes/postRoutes');
@@ -10,6 +16,9 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
+
+app.use('/docs/br', swaggerUI.serve, swaggerUI.setup(swaggerSettingsBr));
+app.use('/docs/en', swaggerUI.serve, swaggerUI.setup(swaggerSettingsEn));
 
 app.use('/login', loginRoutes);
 app.use('/user', userRoutes);
