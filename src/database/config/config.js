c36b88bc74a47ceaec1d7d3692c2eaf10511c72c
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-24 12:08:16 -0300

    :bug: fix: wrong environment variable names

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const options = {
   host: process.env.DB_HOSTNAME,
   port: process.env.DB_PORT,
-  database: process.env.DB_DB_NAME,
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   dialect: 'postgres',
--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const app = require('./api');
 
-// não remova a variável `API_PORT` ou o `listen`
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // não remova esse endpoint
 app.get('/', (_request, response) => {
